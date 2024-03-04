314d057169d755e5cf1d72eea5dbbf5af856aea6
Narzis111 <narzisnahar111@gmail.com>
2024-03-04 13:43:28 +0000

    all done

--- app.js
+++ app.js
@@ -1,32 +1,30 @@
 let count = 0;
 const readCount = document.getElementById("read-count");
 const cardContainer = document.getElementById("card-container");
-const value = document.getElementById("search-box").value;
-const spiner = document.getElementById("spinner");
 
 // ===================All Cards loading====================
 
 const loadCards = (item) => {
-
+  
   const url = 'https://openapi.programming-hero.com/api/retro-forum/posts'
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
       data.posts.forEach((item) => {
-
+        
         cardContainerHtml(item);
       })
     })
 }
 // =======function for append new div for both loadCards and search==========
-function cardContainerHtml(item) {
-
+function cardContainerHtml(item){
+ 
   const postDiv = document.createElement("div");
 
-  postDiv.innerHTML = `
+        postDiv.innerHTML = `
             <div class="flex gap-5 mb-4 p-5 border-2 border-solid rounded-lg  bg-blue-100 border-blue-800  " id="card-container">
               <div class="indicator">
-                <span id="badge" class="indicator-item badge ${(item.isActive) ? 'bg-green-500' : 'bg-red-500'}"></span> 
+                <span id="badge" class="indicator-item badge ${(item.isActive)? 'bg-green-500' : 'bg-red-500'}"></span> 
                 <div class="grid w-32 h-32 bg-base-300 place-items-center"><img src="${item.image}" alt="" srcset=""></div>
               </div>
               <div class="left-discuss">
@@ -52,19 +50,21 @@ function cardContainerHtml(item) {
               </div>
             </div>
             `;
-  cardContainer.appendChild(postDiv);
-}
+        cardContainer.appendChild(postDiv);
+} 
 
 // =============Input-search button with category============
 const handleSearch = async () => {
+  const value = document.getElementById("search-box").value;
+  const spiner = document.getElementById("spinner");
   spiner.classList.remove("hidden");
   const search = value;
   const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${search}`);
   const data = await res.json();
   cardContainer.innerHTML = "";
-  data.posts.forEach((item) => {
+  data.posts.forEach((item)=>{
     cardContainerHtml(item);
-  })
+  }) 
   spiner.classList.add("hidden");
 }
 
@@ -80,14 +80,14 @@ const loadPost = () => {
         div.innerHTML = `
                     <div class="p-10">
           <img src="${item.cover_image}" class="mb-3" srcset="">
-          <p><i class="fa-solid fa-calendar-days"></i> <span>${(item.author.posted_date) ? item.author.posted_date : "Unknown"}</span></p>
+          <p><i class="fa-solid fa-calendar-days"></i> <span>${(item.author.posted_date)? item.author.posted_date : "Unknown"}</span></p>
           <h5 class="my-3 font-extrabold">${item.title}</h5>
             <p>${item.description}</p>
           <div class="flex justify-even mt-3">
               <img class="w-10 h-10 rounded-full" src="${item.profile_image}" alt="" srcset="">
               <div class="ml-4">
               <p class="font-bold">${item.author.name}</p>
-              <p>${(item.author.designation) ? item.author.designation : "Unknown"}</p>               
+              <p>${(item.author.designation)?item.author.designation : "Unknown"}</p>               
             </div>
           </div>       
       </div>
